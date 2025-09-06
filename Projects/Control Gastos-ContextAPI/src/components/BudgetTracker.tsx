ec05c6db60e0409b5a2751724d771367fe09a8f9
Daniel Torres <ruloss29@outlook.es>
2025-09-06 15:57:45 -0500

    feat: add budget management features with context API
    - Implemented BudgetContext for state management of budget and expenses.
    - Created components for budget form, tracker, and expense management.
    - Added SVG icons for health and subscriptions.
    - Integrated date picker for expense date selection.
    - Developed expense modal for adding and editing expenses.
    - Implemented error handling for form submissions.
    - Added utility functions for formatting currency and dates.
    - Established a responsive layout using Tailwind CSS.
    - Included swipeable list functionality for expense details.

---
+++ Projects/Control Gastos-ContextAPI/src/components/BudgetTracker.tsx
@@ -0,0 +1,39 @@
+import { useBudget } from "../hooks/useBudget";
+import AmountDisplay from "./AmountDisplay";
+
+export default function BudgetTracker() {
+    const { state, totalExpenses, available} = useBudget()
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+            <div className="flex justify-center">
+                <img src="/grafico.jpg" alt="Gráfica de gastos" />
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-8">
+                <button
+                    type="button"
+                    className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
+                >
+                    Resetear App
+                </button>
+
+                <AmountDisplay
+                    label="Presupuesto"
+                    amount={state.budget}
+                />
+
+                <AmountDisplay
+                    label="Disponible"
+                    amount={available}
+                />
+
+                <AmountDisplay
+                    label="Gastado"
+                    amount={totalExpenses}
+                />
+            </div>
+
+        </div>
+    )
+}
