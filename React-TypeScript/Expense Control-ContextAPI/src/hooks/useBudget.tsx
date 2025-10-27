4dd169585488a9b31c659942d825a95b6667a6ee
RulosS290 <RulosS29@outlook.es>
2025-10-26 22:44:34 -0500

    Reorganize

--- React-TypeScript/Expense Control-ContextAPI/src/hooks/useBudget.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react"
-import { BudgetContext } from "../context/BudgetContext"
-
-export const useBudget = () => {
-    const context = useContext(BudgetContext)
-    return context
-}
