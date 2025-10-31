d92189d07b331af52dc8d61f30ab68ad3bacd921
RulosS290 <RulosS29@outlook.es>
2025-10-31 17:57:49 -0500

    feat: Add main components for climate application including form, alert, spinner, and weather detail
    - Created App.module.css for main application styling.
    - Implemented Alert component with styling for error messages.
    - Developed Form component to handle user input for city and country.
    - Added Spinner component for loading indication.
    - Created WeatherDetail component to display weather information.
    - Introduced countries data for dropdown selection in the form.
    - Implemented useWeather hook for fetching weather data from API.
    - Added utility function for temperature formatting.
    - Defined types for search parameters, countries, and weather data.

--- React-TypeScript/Projects/Climate Application/src/App.tsx
+++ React-TypeScript/Projects/Climate Application/src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from "./App.module.css"
+import Alert from "./components/Alert/Alert"
+import Form from "./components/Form/Form"
+import Spinner from "./components/Spinner/Spinner"
+import WeatherDetail from "./components/WeatherDetail/WeatherDetail"
+import useWeather from "./hooks/useWeather"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { weather, loading, notFound, fetchWeather, hasWeatherData } = useWeather()
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className={styles.title}>Weather Finder</h1>
+
+      <div className={styles.container}>
+        <Form fetchWeather = {fetchWeather} />
+        {loading && <Spinner/>}
+
+        {hasWeatherData && <WeatherDetail weather={weather}/> }
+
+        {notFound && <Alert>City not found</Alert>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
